f89bc6153560ffb37eb51e59f42ef8465c78465b
Holmes1204 <HolmeszHui@gmail.com>
2021-02-22 18:07:16 +0800

    forward work

--- main.cpp
+++ main.cpp
@@ -5,24 +5,36 @@
 #include <opencv2/imgproc.hpp>
 using namespace std;
 using namespace cv;
+
+/**
+ * @brief  loading image
+ * 
+ * @param src_path 
+ * @param binary_img
+ * @return bool 
+ */
+bool loadImage(const char * src_path, cv::Mat& img)
+{
+	// 原图以灰度图形式加载
+	// 以及加载异常的处理
+	img = cv::imread(src_path);
+	if (img.empty())
+	{
+		std::cout << "IMAGE LOAD FAILDED!"<< std::endl;
+		return false;
+	}
+	return true;
+}
+
 int main()
 {
-	//打开一个默认的相机
-	VideoCapture capture(0);
-	//检查是否成功打开
-	if (!capture.isOpened())
-		return -1;
-	Mat edges;
-	while (1)
-	{
-		Mat frame;
-		capture >> frame;					 //从相机读取新一帧
-		cvtColor(frame, edges, CV_BGR2GRAY); //变为灰度图
-		blur(edges, edges, Size(3, 3));		 //均值滤波降噪
-		Canny(edges, edges, 5, 38, 3);		 //canny算子
-		imshow("被canny后的视频", edges);	 //显示
-		if (waitKey(1) == 'q')
-			break;
-	}
+    char src[]="/home/holmes/Engineer_Competition/Obeject_Detect/test.jpg";
+    cv::Mat img,grey;
+    if(!loadImage(src,img))return -1;
+    cv::cvtColor(img,grey,CV_BGR2GRAY);
+    cv::adaptiveThreshold(grey,img,255, 0, 1, 101, 10);
+    cv::imshow("show",img);
+    waitKey(0);
+    cv::destroyAllWindows();
 	return 0;
 }
